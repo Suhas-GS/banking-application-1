bb9c88d98cd2d6aa2376b3e999c68111b34da645
Suhas-GS <suhasgs16@gmail.com>
2024-11-21 10:58:37 +0530

    appwrite implementation part1

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -23,12 +23,13 @@ import CustomInput from './CustomInput';
 import { authFormSchema } from '@/lib/utils';
 import { Loader2 } from 'lucide-react';
 import { useRouter } from 'next/navigation';
-import { signIn, signUp } from '@/lib/actions/user.actions';
+import { getLoggedInUser, signIn, signUp } from '@/lib/actions/user.actions';
 
 const AuthForm = ({ type }: { type: string }) => {
     const router = useRouter();
     const [user, setUser] = useState(null)
     const [isLoading, setIsLoading] = useState(false)
+    
 
     const formSchema = authFormSchema(type);
 
@@ -46,23 +47,24 @@ const AuthForm = ({ type }: { type: string }) => {
         setIsLoading(true)
         try {
 
-            console.log(data);
+            // console.log(data);
             
             // sign up with appwrite & create plaid token
-            // if (type === 'sign-up') {
-            //     const newUser = await signUp(data);
-            //     setUser(newUser);
-            // }
-            // if (type === 'sign-in') {
-            //     const response = await signIn({
-            //         email: data.email,
-            //         password: data.password,
-            //     })
+            if (type === 'sign-up') {
+                const newUser = await signUp(data);
+                setUser(newUser);
+            }
 
-            //     if (response) {
-            //         router.push('/')
-            //     }
-            // }
+            if (type === 'sign-in') {
+                // const response = await signIn({
+                //     email: data.email,
+                //     password: data.password,
+                // })
+
+                // if (response) {
+                //     router.push('/')
+                // }
+            }
         }
         catch (error) {
             console.log(error);
